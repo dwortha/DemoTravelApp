f1ac75ed04450b740ace648cb58d86eb62131854
dwortham <dariuswortham@icloud.com>
2025-09-02 16:49:44 -0400

    gbg

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DemoTravelApp/'
+  base: '/DemoTravelApp/' // Set the base path for GitHub Pages
 })
